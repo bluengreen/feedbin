26b39312fc96249f78c65ff9b116585966ecfba6
Phillip Novess <phillip@novess.com>
2021-06-21 23:15:04 -0500

    plow the front away

--- app/controllers/pages_internal_controller.rb
+++
@@ -1,7 +0,0 @@
-class PagesInternalController < ApplicationController
-
-  def create
-    @entry = SavePage.new.perform(current_user.id, params[:url], nil)
-    get_feeds_list
-  end
-end
--- app/views/feeds/_feed.xml.builder
+++
@@ -1,2 +0,0 @@
-title = titles[feed.id] || feed.title
-xml.outline text: title, title: title, type: "rss", xmlUrl: feed.feed_url, htmlUrl: feed.site_url
